1a81209cee24af1c0a256e70a7c98e29813bf1b5
quietroom-dev <keitai0113@gmail.com>
2026-07-31 10:26:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// 写真を選択
+const photoInput = document.getElementById("photoInput");
 
+document.getElementById("uploadBtn").onclick = () => {
+    photoInput.click();
+};
+
+photoInput.onchange = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    alert("選択した写真\n\n" + file.name);
+};
+
+// ランキング
+document.getElementById("rankingBtn").onclick = () => {
+    alert("ランキングは後で作ります");
+};
+
+// 占い
+document.getElementById("fortuneBtn").onclick = () => {
+    alert("今日の謎写真占いは後で作ります");
+};
