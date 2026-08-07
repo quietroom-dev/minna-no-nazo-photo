bdb3f8b23060d45f6f52d1d5642f1322de6ec7cb
quietroom-dev <keitai0113@gmail.com>
2026-08-07 11:10:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -195,9 +195,9 @@ white-space:nowrap;
 
 <span id="like-${photoId}" style="white-space:nowrap;"><span style="font-size:10px;">👍</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.like}</span></span>
 <span id="happy-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-3px;">😆</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.happy}</span></span>
-<span id="sad-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-3px;">😢</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.sad}</span></span>
-<span id="angry-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-3px;">😡</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.angry}</span></span>
-<span id="mystery-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-3px;">❓</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.mystery}</span></span>
+<span id="sad-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-5px;">😢</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.sad}</span></span>
+<span id="angry-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-7px;">😡</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.angry}</span></span>
+<span id="mystery-${photoId}" style="white-space:nowrap;"><span style="font-size:10px; margin-left:-10px;">❓</span><small style="font-size:5px;">×</small><span style="font-size:7px;">${photo.mystery}</span></span>
 
 </div>
 
